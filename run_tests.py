828548a8b0600b1a4672bb204bf7f4cc766fa230
Adriano Schommer <p0092549@brookes.ac.uk>
2021-11-26 12:06:05 +0000

    add filtered temp before set_test_data

--- run_tests.py
+++ run_tests.py
@@ -14,18 +14,18 @@ from scipy.optimize import least_squares
 import numpy as np
 
 raw_data = pd.DataFrame(DataParse().read_file(0))
-
+raw_data = DataParse().add_filtered_temp(raw_data, 131, 1)
 #INITIAL_CUTOFF = 27840
 # File 0:
 #INITIAL_CUTOFF = 24150
 
 test_data = DataParse().set_test_data(raw_data, 24150)
 #DataParse().plot_test_data(test_data)
-test_data = DataParse().add_filtered_data(test_data, 131, 1)
+
 AVG_Q_GEN = DataParse().calculate_AVG_Q_GEN(test_data, 3.82)
 final_section_data = DataSection().set_final_section(test_data, 0.3)
 R_OUT = DataSection().calculate_R_OUT(final_section_data, AVG_Q_GEN)
-#DataSection().plot_final_section(section_final_data)
+#DataSection().plot_final_section(final_section_data)
 AVG_SURFACE_TEMP = DataSection().calculate_AVG_SURFACE_TEMP(final_section_data)
 
 ## if you calculate AVG_Q_GEN only from the final data you get lower values
@@ -34,9 +34,9 @@ AVG_Q_GEN = DataParse().calculate_AVG_Q_GEN(final_section_data, 3.82)
 
 initial_section_data = DataSection().set_initial_section(
     test_data, 0.95, AVG_SURFACE_TEMP)
-#DataSection().plot_initial_section(initial_section_data)
+DataSection().plot_initial_section(initial_section_data)
 ys = initial_section_data['surface_temp_filtered']
-ys = initial_section_data['Aux_Temperature_1(C)']
+#ys = initial_section_data['Aux_Temperature_1(C)']
 #time_data = initial_section_data['Test_Time(s)']
 opt_result = DataSection().optimize_equation(ys, initial_section_data,
                            AVG_Q_GEN, R_OUT)
