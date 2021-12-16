a3c1df39822e181534e802b7a94e3e39b1eb8668
Adriano Schommer <adriano.schommer@gmail.com>
2021-12-16 10:20:42 +0000

    added OCV dependency on SOC

--- run_tests.py
+++ run_tests.py
@@ -20,19 +20,17 @@ raw_data = DataParse().add_temperature_channels(raw_data, 151, 1)
 raw_data = DataParse().add_SOC_channel(raw_data)
 file = 'OCV_SOC.csv'
 raw_data = DataParse().add_OCV_channel(raw_data, file)
-#INITIAL_CUTOFF = 27840
-# File 0:
-#INITIAL_CUTOFF = 24150
 test_data = DataParse().set_test_data(raw_data, 20)
 DataParse().plot_test_data(test_data)
-AVG_Q_GEN = DataParse().calculate_AVG_Q_GEN(test_data, 3.82)
+OCV = test_data.OCV
 final_section_data = DataSection().set_final_section(test_data, 0.3)
-R_OUT_no_fan = DataSection().calculate_R_OUT(final_section_data, AVG_Q_GEN)
+
 #R_OUT_no_fan = 2.27
 DataSection().plot_final_section(final_section_data)
 AVG_SURFACE_TEMP = DataSection().calculate_AVG_SURFACE_TEMP(final_section_data)
 ## if you calculate AVG_Q_GEN only from the final data you get lower values
-AVG_Q_GEN = DataParse().calculate_AVG_Q_GEN(final_section_data, 3.82)
+AVG_Q_GEN = DataParse().calculate_AVG_Q_GEN(final_section_data, OCV)
+R_OUT_no_fan = DataSection().calculate_R_OUT(final_section_data, AVG_Q_GEN)
 initial_section_data = DataSection().set_initial_section(
     test_data, 0.93, AVG_SURFACE_TEMP)
 DataSection().plot_initial_section(initial_section_data)
@@ -51,24 +49,20 @@ raw_data = DataParse().add_temperature_channels(raw_data, 151, 1)
 raw_data = DataParse().add_SOC_channel(raw_data)
 file = 'OCV_SOC.csv'
 raw_data = DataParse().add_OCV_channel(raw_data, file)
-#INITIAL_CUTOFF = 27840
-# File 0:
-#INITIAL_CUTOFF = 24150
 test_data = DataParse().set_test_data(raw_data, 20)
 DataParse().plot_test_data(test_data)
-AVG_Q_GEN = DataParse().calculate_AVG_Q_GEN(test_data, 3.82)
+OCV = test_data.OCV
 final_section_data = DataSection().set_final_section(test_data, 0.3)
-R_OUT_fan = DataSection().calculate_R_OUT(final_section_data, AVG_Q_GEN)
-#R_OUT_fan = 1.77
+
 DataSection().plot_final_section(final_section_data)
 AVG_SURFACE_TEMP = DataSection().calculate_AVG_SURFACE_TEMP(final_section_data)
-## if you calculate AVG_Q_GEN only from the final data you get lower values
-AVG_Q_GEN = DataParse().calculate_AVG_Q_GEN(final_section_data, 3.82)
+
+AVG_Q_GEN = DataParse().calculate_AVG_Q_GEN(final_section_data, OCV)
+R_OUT_fan = DataSection().calculate_R_OUT(final_section_data, AVG_Q_GEN)
 initial_section_data = DataSection().set_initial_section(
     test_data, 0.93, AVG_SURFACE_TEMP)
 DataSection().plot_initial_section(initial_section_data)
 ys = initial_section_data['surface_temp_filtered']
-#ys = initial_section_data['Aux_Temperature_1(C)']
 opt_result_fan = DataSection().optimize_equation(ys, initial_section_data,
                            AVG_Q_GEN, R_OUT_fan, plot=True)
 
